a18f597e9fff9144305041ea5970456ff1a8c920
Епифанов Егор <egorbotan@gmail.com>
2022-04-22 17:41:13 +0300

    Added "HI" to check

--- main.py
+++ main.py
@@ -4,8 +4,4 @@ a = float(input("Укажите, чему равно ребро куба "))
 print("Объем куба равен ", math_func.cube(a))
 
 a, b = input("Укажите катеты прямоуг. треуг. через пробел ").strip().split()
-print("Гипотенуза ", math_func.hipot(float(a), float(b)))
-
-
-
-
+print("Гипотенуза ", math_func.hipot(float(a), float(b)), "HI")
